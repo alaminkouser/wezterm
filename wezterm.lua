afb7432bb39aca2a6274b31f9b5d6622225d493f
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-06-21 17:09:19 +0600

    config

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
@@ -7,11 +7,12 @@ config.window_decorations = "RESIZE"
 config.font_size = 18.0
 config.use_fancy_tab_bar = false
 config.adjust_window_size_when_changing_font_size = false
-config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font "NotoMonoNerdFont"
 config.default_cwd = "."
 config.initial_cols = 120
 config.initial_rows = 24
+config.default_cursor_style = "BlinkingBar"
+config.tab_max_width = 16
 config.window_padding = {
 	left   = 0,
 	right  = 0,
@@ -19,14 +20,14 @@ config.window_padding = {
 	bottom = 0,
 }
 config.window_frame = {
-  border_left_width = "2pt",
-  border_right_width = "2pt",
-  border_bottom_height = "2pt",
-  border_top_height = "2pt",
-  border_left_color = "grey",
-  border_right_color = "grey",
-  border_bottom_color = "grey",
-  border_top_color = "grey",
+	border_left_width = "2pt",
+	border_right_width = "2pt",
+	border_bottom_height = "2pt",
+	border_top_height = "2pt",
+	border_left_color = "grey",
+	border_right_color = "grey",
+	border_bottom_color = "grey",
+	border_top_color = "grey",
 }
 config.colors = {
 	cursor_bg = "#ffffff",
@@ -34,8 +35,42 @@ config.colors = {
 	selection_fg = "#000000",
 	selection_bg = "#ffffff",
 	tab_bar = {
-		background = "rgba(0,0,0,0.8)",
+		background = "rgba(0,0,0,0.85)",
 	}
 }
+
+wezterm.on("update-status", function(window)
+	local overrides = window:get_config_overrides() or {}
+	if window:is_focused() then
+		overrides.window_frame = {
+			border_left_width = "2pt",
+			border_right_width = "2pt",
+			border_bottom_height = "2pt",
+			border_top_height = "2pt",
+			border_left_color = "grey",
+			border_right_color = "grey",
+			border_bottom_color = "grey",
+			border_top_color = "grey",
+		}
+	else
+		overrides.window_frame = {
+			border_left_width = "2pt",
+			border_right_width = "2pt",
+			border_bottom_height = "2pt",
+			border_top_height = "2pt",
+			border_left_color = "black",
+			border_right_color = "black",
+			border_bottom_color = "black",
+			border_top_color = "black",
+		}
+	end
+	window:set_config_overrides(overrides)
+end)
+
+wezterm.on("update-right-status", function(window, pane)
+    window:set_right_status(wezterm.format {
+        {Text = pane:get_current_working_dir()}
+    })
+end)
 
 return config
