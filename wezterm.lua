34b7eadcf6581c1cdaae5a1de3f6fbb3f9e22318
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-06-30 01:26:33 +0600

    update

--- wezterm.lua
+++ wezterm.lua
@@ -14,12 +14,15 @@ config.initial_rows = 24
 config.default_cursor_style = "BlinkingBar"
 config.tab_max_width = 16
 config.use_resize_increments = true
+config.hide_tab_bar_if_only_one_tab = true
+
 config.window_padding = {
 	left   = 0,
 	right  = 0,
 	top    = 0,
 	bottom = 0,
 }
+
 config.colors = {
 	cursor_bg = "#ffffff",
 	cursor_border = "#ffffff",
@@ -32,7 +35,18 @@ config.colors = {
 
 wezterm.on("update-status", function(window)
 	local overrides = window:get_config_overrides() or {}
-	if window:is_focused() then
+	if window:get_dimensions().is_full_screen then
+		overrides.window_frame = {
+			border_left_width = "0pt",
+			border_right_width = "0pt",
+			border_bottom_height = "0pt",
+			border_top_height = "0pt",
+			border_left_color = "black",
+			border_right_color = "black",
+			border_bottom_color = "black",
+			border_top_color = "black",
+		}
+	elseif window:is_focused() then
 		overrides.window_frame = {
 			border_left_width = "2pt",
 			border_right_width = "2pt",
@@ -59,12 +73,19 @@ wezterm.on("update-status", function(window)
 end)
 
 wezterm.on("update-right-status", function(window, pane)
-    window:set_right_status(wezterm.format {
+	window:set_right_status(wezterm.format {
         {
 			Text = pane:get_foreground_process_name():match("[\\/]([^\\/]-)$")
 				or pane:get_foreground_process_name()
 		}
     })
 end)
+
+config.keys = {
+	{
+		key = "F11",
+		action = wezterm.action.ToggleFullScreen
+	}
+}
 
 return config
